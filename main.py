687686e6be0fede2996f10d39dfdb5821530057d
Ahmad <huntertom417@gmail.com>
2021-07-26 23:21:55 +0500

    Moved inventory and item to separate file, added functionality

--- main.py
+++ main.py
@@ -1,47 +1,5 @@
 import json
-
-
-class Item:
-    def __init__(self, name: str, quantity: int, description=""):
-        self.name = name
-        self.quantity = quantity
-        self.description = description
-
-
-class Inventory:
-    def __init__(self, path=""):
-        #id 0 stores the free ids that can be used to store items
-        self.inventory = {0: []}
-        self._nextId = 1
-        if path:
-            self.loadInventory(path)
-
-    def addItem(self, item: Item):
-        if self.inventory[0] == []:
-            # noinspection PyTypeChecker
-            self.inventory[self._nextId] = {"name": item.name,
-                                            "quantity": item.quantity,
-                                            "description": item.description}
-            self._nextId += 1
-        else:
-            temp = self.inventory[0].pop()
-            # noinspection PyTypeChecker
-            self.inventory[temp] = {"name": item.name,
-                                    "quantity": item.quantity,
-                                    "description": item.description}
-
-    def removeItem(self, item_id: int):
-        self.inventory[item_id] = {}
-        self.inventory[0].append(item_id)
-
-    def loadInventory(self, path: str):
-        pass
-
-    def __str__(self):
-        inv_str = "Showing listing for inventory\n" + "-"*10 + "\n"
-
-        return self.inventory.__str__()
-
+from inventory import Inventory, Item
 
 apple = Item(name="apple",quantity=10)
 cpu = Item(name="9900k",quantity=12,description="brand new")
@@ -55,8 +13,9 @@ inv.addItem(gpu)
 
 print(inv)
 
-inv.removeItem(2)
+inv.removeItem(3)
 print(inv)
 
 inv.addItem(ram)
+inv.editItem(1,name="mango")
 print(inv)
